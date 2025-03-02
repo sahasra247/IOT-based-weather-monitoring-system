5e7e026a07aa0b340c3f04207e78f68afc6c37d7
sahasra247 <pulumati.sahasra@gmail.com>
2025-03-02 19:37:34 +0530

    Create ESP-32.ino

---
+++ ESP-32/ESP-32.ino
@@ -0,0 +1,54 @@
+#include <WiFi.h>
+#include "DHTesp.h"
+#include "ThingSpeak.h"
+
+const int DHT_PIN = 15;
+const int LED_PIN = 13;
+const char* WIFI_NAME = "Wokwi-GUEST";
+const char* WIFI_PASSWORD = "";
+const int myChannelNumber = 2518599 ;
+const char* myApiKey = "G7G8I24ZYYGM9FXE";
+const char* server = "api.thingspeak.com";
+
+DHTesp dhtSensor;
+WiFiClient client;
+
+void setup() {
+  Serial.begin(115200);
+  dhtSensor.setup(DHT_PIN, DHTesp::DHT22);
+  pinMode(LED_PIN, OUTPUT);
+  WiFi.begin(WIFI_NAME, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED){
+    delay(1000);
+    Serial.println("Wifi not connected");
+  }
+  Serial.println("Wifi connected !");
+  Serial.println("Local IP: " + String(WiFi.localIP()));
+  WiFi.mode(WIFI_STA);
+  ThingSpeak.begin(client);
+}
+
+void loop() {
+  TempAndHumidity  data = dhtSensor.getTempAndHumidity();
+  ThingSpeak.setField(1,data.temperature);
+  ThingSpeak.setField(2,data.humidity);
+  if (data.temperature > 35 || data.temperature < 12 || data.humidity > 70 || data.humidity < 40) {
+    digitalWrite(LED_PIN, HIGH);
+  }else{
+    digitalWrite(LED_PIN, LOW);
+  }
+  
+  int x = ThingSpeak.writeFields(myChannelNumber,myApiKey);
+  
+  Serial.println("Temp: " + String(data.temperature, 2) + "°C");
+  Serial.println("Humidity: " + String(data.humidity, 1) + "%");
+  
+  if(x == 200){
+    Serial.println("Data pushed successfull");
+  }else{
+    Serial.println("Push error" + String(x));
+  }
+  Serial.println("---");
+
+  delay(10000);
+}
